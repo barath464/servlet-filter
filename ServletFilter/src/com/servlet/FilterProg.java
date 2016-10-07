2431c560484a2c864796a9f4c1eb78039cb3c52b
bharath <bharathreddy.kotha@gmail.com>
2016-10-07 11:54:55 -0400

    first commit for filter

---
+++ ServletFilter/src/com/servlet/FilterProg.java
@@ -0,0 +1,46 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class FilterProg
+ */
+
+public class FilterProg implements Filter {
+
+   
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("Filter is called......");
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+		
+		if(httpServletRequest.getRequestURI().contains("login")){
+			chain.doFilter(request, response);
+		}else{
+			//Login logic check
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
